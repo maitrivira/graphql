6ea4be280544b87dc2c908b0271102403ac0bcc5
Maitri Vira <maitrivira2@gmail.com>
2019-09-20 10:30:58 +0700

    second commot

--- server/app.js
+++ server/app.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+//allow cross-origin requests
+app.use(cors())
+
+mongoose.connect('mongodb+srv://maitrivira:Tokekbelang2@cluster0-tcno2.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+mongoose.connection.once('open', ()=>{
+    console.log('connected to database');
+});
 
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }))
 
-app.listen(4000, () => {
-    console.log('now listening for requests on port 4000');
+app.listen(3000, () => {
+    console.log('now listening for requests on port 3000');
 });
